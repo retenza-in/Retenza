13b8c5f15c7a88e5a7d4747aec544cfcdacdd8f3
megaadev <akshay6.nitt@gmail.com>
2025-08-18 17:58:30 +0530

    fix: update PWA config to prevent build manifest caching issues

--- next.config.js
+++ next.config.js
@@ -6,6 +6,34 @@ const config = withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /\/_next\/.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'next-cache',
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        },
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+      },
+    },
+    {
+      urlPattern: /\/api\/.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'api-cache',
+        expiration: {
+          maxEntries: 16,
+          maxAgeSeconds: 60 * 60, // 1 hour
+        },
+      },
+    },
+  ],
 })({
   reactStrictMode: true,
   experimental: {
